9de14544627fb79580cd5903bca278733605d776
arafat551 <kondiraf551@gmail.com>
2025-05-28 01:58:32 +0300

    premier push

---
+++ src/main/java/NEW/Testing/App.java
@@ -0,0 +1,10 @@
+package NEW.Testing;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("je suis le nouveau code!");
+    }
+}
